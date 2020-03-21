44d637c33c33e485d1245ff9e56b27177b49f163
NMaslenko <nvmaslenko@gmail.com>
2020-03-21 22:27:19 +0500

    add file constant.py for saving all constants for project

---
+++ sub/constant.py
@@ -0,0 +1,8 @@
+class Constant:
+	TRANSLATOR_REFERENCE = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
+	TRANSLATOR_API_KEY = 'trnsl.1.1.20200321T130137Z.ef5b35dd74f0ce01.173bcb1f6adb11f7fc34cae5cd75c6ecf2207261'
+	RUS = 'ru'
+	ENG = 'en'
+	ESP = 'es'
+	FRA = 'fr'
+
