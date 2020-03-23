ba14c1093bdc518765097aaf01408221bcfcfad2
NMaslenko <nvmaslenko@gmail.com>
2020-03-23 14:50:04 +0500

    -- remove all constants and replace it to .sub/constants.py -- redact code to appropriate to pep8 -- final testing

--- telegram_core.py
+++ telegram_core.py
@@ -1,5 +1,4 @@
 import telebot
-from telebot import types
 from sub.constant import Constant
 from sub.options import UserOptions
 import API_tester
@@ -9,22 +8,24 @@ bot = telebot.TeleBot(Constant.TELEGRAM_API_KEY)
 
 @bot.inline_handler(func=lambda query: len(query.query) > 0)
 def translate_query(query):
-    proposal = []
-    try:
-        for option in range(6):
-            proposal.append(telebot.types.InlineQueryResultArticle(
-            id=UserOptions.options['id'][option],
-            title=UserOptions.options['title'][option],
-            description=UserOptions.options['description'][option],
-            thumb_url=UserOptions.options['thumb_url'][option], thumb_width=48, thumb_height=48,
-            input_message_content=telebot.types.InputTextMessageContent(
-            message_text=f'send by {query.from_user.first_name} (@{query.from_user.username})\n'+
-                         API_tester.get_translate(UserOptions.options['message_text'][option][0],
-                                                  UserOptions.options['message_text'][option][1], query.query)))
-            )
-        bot.answer_inline_query(query.id, proposal, cache_time=10)
-    except Exception:
-        print(Exception)
+	proposal = []
+	try:
+		for option in range(len(UserOptions.options['id'])):
+			proposal.append(telebot.types.InlineQueryResultArticle(
+				id=UserOptions.options['id'][option],
+				title=UserOptions.options['title'][option],
+				description=UserOptions.options['description'][option],
+				thumb_url=UserOptions.options['thumb_url'][option], thumb_width=48, thumb_height=48,
+				input_message_content=telebot.types.InputTextMessageContent(
+					message_text=f'send by {query.from_user.first_name} (@{query.from_user.username})\n' +
+					             API_tester.get_translate(UserOptions.options['message_text'][option][0],
+					                                      UserOptions.options['message_text'][option][1], query.query)))
+			)
+
+		bot.answer_inline_query(query.id, proposal, cache_time=10)
+	except Exception:
+		print(Exception)
+
 
 while True:
-    bot.polling(none_stop=True, interval=0, timeout=0)
+	bot.polling(none_stop=True, interval=0, timeout=0)
